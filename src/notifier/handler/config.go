2268d5b104cc5657976e216f99c475d95cc00510
Pedro Guilherme Rohr <pedro.guilherme.rohr@gmail.com>
2021-12-06 13:14:43 +0000

    feat: add an http call to the telegram endpoint responsible for sending messages

--- src/notifier/handler/config.go
+++ src/notifier/handler/config.go
@@ -1,17 +1,28 @@
 package handler
 
 import (
+	"log"
 	"os"
+	"strconv"
 )
 
 type config struct {
 	deteBotApiToken string
-	deteChatId      string
+	deteChatId      int64
 }
 
 func NewConfigFromEnv() *config {
 	return &config{
 		deteBotApiToken: os.Getenv("DETE_BOT_API_TOKEN"),
-		deteChatId:      os.Getenv("DETE_CHAT_ID"),
+		deteChatId:      convertChatIdToInt64(os.Getenv("DETE_CHAT_ID")),
 	}
+}
+
+func convertChatIdToInt64(chatId string) int64 {
+	result, err := strconv.ParseInt(os.Getenv("DETE_CHAT_ID"), 10, 64)
+	if err != nil {
+		log.Fatalf("Got error converting Telegram chat ID to int64: %s", err)
+	}
+
+	return result
 }
--- src/notifier/handler/handler.go
+++ src/notifier/handler/handler.go
@@ -1,9 +1,24 @@
 package handler
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
+)
+
+const (
+	baseURL             = "https://api.telegram.org/bot"
+	sendMessageEndpoint = "sendMessage"
+	applicationType     = "application/json"
+	baseNotification    = "A date has been updated!"
+	briefcaseEmoji      = "💼"
+	calendarEmoji       = "📅"
+	dateFormat          = "2 January 2006"
 )
 
 type Handler interface {
@@ -12,7 +27,12 @@ type Handler interface {
 
 type lambdaHandler struct {
 	deteBotApiToken string
-	deteChatId      string
+	deteChatId      int64
+}
+
+type sendMessageReqBody struct {
+	ChatId int64  `json:"chat_id"`
+	Text   string `json:"text"`
 }
 
 func (l lambdaHandler) Run(e events.DynamoDBEvent) {
@@ -25,12 +45,48 @@ func (l lambdaHandler) Run(e events.DynamoDBEvent) {
 		newDateType := record.Change.NewImage["Type"].String()
 		newDate := record.Change.NewImage["Date"].String()
 		log.Printf("New - %s: %s\n", newDateType, newDate)
+
+		text := getNotificationText(newDateType, newDate)
+		l.sendTelegramMessage(text)
 	}
 }
 
-func NewLambdaHandler(deteBotApiToken string, deteChatId string) *lambdaHandler {
+func NewLambdaHandler(deteBotApiToken string, deteChatId int64) *lambdaHandler {
 	return &lambdaHandler{
 		deteBotApiToken: deteBotApiToken,
 		deteChatId:      deteChatId,
 	}
+}
+
+func (l lambdaHandler) sendTelegramMessage(text string) {
+	reqBody := &sendMessageReqBody{
+		ChatId: l.deteChatId,
+		Text:   text,
+	}
+	reqBytes, err := json.Marshal(reqBody)
+	if err != nil {
+		log.Fatalf("Got error marshalling sendMessage request body: %s", err)
+	}
+
+	uri := fmt.Sprintf("%s%s/%s", baseURL, l.deteBotApiToken, sendMessageEndpoint)
+	res, err := http.Post(uri, applicationType, bytes.NewBuffer(reqBytes))
+	if err != nil {
+		log.Fatalf("Got error posting sendMessage request: %s", err)
+	}
+	if res.StatusCode != http.StatusOK {
+		log.Fatalf("Got unexpected status after sending message to Telegram: %s", err)
+	}
+}
+
+func getNotificationText(dateType, date string) string {
+	return fmt.Sprintf("```\n%s\n\n%s %s\n\n%s %s\n```", baseNotification, briefcaseEmoji, dateType, calendarEmoji, formatDate(date))
+}
+
+func formatDate(fullDateTime string) string {
+	date, err := time.Parse(time.RFC3339, fullDateTime)
+	if err != nil {
+		log.Fatalf("Got error parsing processing date: %s", err)
+	}
+
+	return date.Format(dateFormat)
 }
